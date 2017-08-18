37ca318b945d6d31be80648142b45e35e01496f6
William Pelrine <williampelrine@Williams-MacBook-Pro.local>
2017-08-18 16:08:27 -0500

    Show debug dialog from js

--- www/common.js
+++ www/common.js
@@ -1,8 +1,16 @@
 var exec = require('cordova/exec');
 
+/**
+ * LIVE UPDATE API
+ * 
+ * The plugin API for the live updates feature.
+ */
 var IonicDeploy = {
   init: function(app_id, server_host, success, failure) {
     exec(success, failure, 'IonicDeploy', 'initialize', [app_id, server_host]);
+  },
+  debug: function(success, failure) {
+    exec(success, failure, 'IonicDeploy', 'showDebug', []);
   },
   check: function(app_id, channel_tag, success, failure) {
     exec(success, failure, 'IonicDeploy', 'check', [app_id, channel_tag]);
@@ -33,6 +41,12 @@ var IonicDeploy = {
   },
 };
 
+/**
+ * BASE API
+ * 
+ * All features of the Ionic Cordova plugin are registered here, along with some low level error tracking features used
+ * by the monitoring service.
+ */
 var IonicCordova = {
   enableCrashLogging: function(success, fail) {
     exec(success, fail, "IonicCordovaCommon", "enableCrashLogging");
@@ -51,5 +65,20 @@ var IonicCordova = {
   },
   deploy: IonicDeploy
 };
+
+/**
+ * DEBUG DIALOG
+ * 
+ * Right now this is just used for live updates, but all manner of functionality could be added in the case where the
+ * app is a debug build.
+ */
+function onDeviceReady() {
+  IonicCordova.deploy.debug(function(success) {
+    console.log("SUCCESS", success);
+  }, function(fail) {
+    console.log("FAIL", fail);
+  })
+}
+document.addEventListener("deviceready", onDeviceReady, false);
 
 module.exports = IonicCordova;
