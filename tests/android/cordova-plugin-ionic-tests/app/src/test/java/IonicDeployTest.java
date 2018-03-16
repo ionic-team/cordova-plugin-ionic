98e7626a9ef1773394c77053e6b5fade511f89a9
William Pelrine <rudy@ionic.io>
2018-03-16 16:57:09 -0500

    Empty android test example

---
+++ tests/android/cordova-plugin-ionic-tests/app/src/test/java/IonicDeployTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by williampelrine on 3/16/18.
+ */
+public class IonicDeployTest {
+    @Test
+    public void execute() throws Exception {
+    }
+
+    @Test
+    public void showDebug() throws Exception {
+    }
+
+}
