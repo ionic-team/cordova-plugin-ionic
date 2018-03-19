5adc53b7d5b0c6d235583c36d74b7e71796a3ce7
William Pelrine <rudy@ionic.io>
2018-03-19 16:43:37 -0500

    Functional tests with mocks

--- tests/android/cordova-plugin-ionic-tests/app/src/test/java/IonicDeployTest.java
+++ tests/android/cordova-plugin-ionic-tests/app/src/test/java/IonicDeployTest.java
@@ -1,17 +1,25 @@
+import com.ionicframework.deploy.IonicDeploy;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by williampelrine on 3/16/18.
  */
 public class IonicDeployTest {
     @Test
-    public void execute() throws Exception {
+    public void showDebug() throws Exception {
+        new IonicDeploy();
     }
 
     @Test
-    public void showDebug() throws Exception {
+    public void showDebugTwo() throws Exception {
+        new IonicDeploy();
+    }
+
+    @Test
+    public void execute() throws Exception {
+        new IonicDeploy();
     }
 
 }
