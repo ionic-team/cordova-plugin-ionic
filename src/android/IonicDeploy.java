148c4134cbbda234d59dc6aab567be9eb615732b
William Pelrine <rudy@ionic.io>
2018-03-21 15:37:44 -0500

    Add plugin version constants

--- src/android/IonicDeploy.java
+++ src/android/IonicDeploy.java
@@ -97,6 +97,7 @@ public class IonicDeploy extends CordovaPlugin {
   private static ProgressDialog spinnerDialog;
   private ImageView splashImageView;
 
+  public static final String PLUGIN_VERSION = "4.2.0";
   public static final String INDEX_UPDATED = "INDEX_UPDATED";
   public static final String NO_DEPLOY_LABEL = "NO_DEPLOY_LABEL";
   public static final String NO_DEPLOY_AVAILABLE = "NO_DEPLOY_AVAILABLE";
