57c79960badbd22691c68a869ec757934aae93c8
Giovanni Di Milia <giovanni@ionic.io>
2018-05-03 11:55:45 -0400

    Removed old ios code

--- src/old_ios/IonicConstant.m
+++
@@ -1,5 +0,0 @@
-#import "IonicConstant.h"
-
-NSString *const NO_DEPLOY_LABEL = @"NO_DEPLOY_LABEL";
-NSString *const NOTHING_TO_IGNORE = @"NOTHING_TO_IGNORE";
-NSString *const PLUGIN_VERSION = @"4.1.3";
--- src/old_ios/IonicCordovaCommon.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-
-@interface IonicCordovaCommon : CDVPlugin
-
-- (void)getAppInfo:(CDVInvokedUrlCommand*)command;
-
-- (void) getPreferences:(CDVInvokedUrlCommand *)command;
-
-@end
