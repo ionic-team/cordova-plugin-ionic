db10ab260d36c5939f8a55997891952b116dc4f6
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2018-07-26 11:33:31 -0500

    fix code version bump script

--- update-plugin-version-code.sh
+++ update-plugin-version-code.sh
@@ -2,9 +2,10 @@
 set -f errexit
 
 # Script vars
-VERSION=$(npm list @ionic/pro-jobs | grep "@" | cut -d "@" -f 3 | cut -d " " -f 1)
+VERSION=$(npm list cordova-plugin-ionic | grep "@" | cut -d "@" -f 2 | cut -d " " -f 1);
+echo "updating PLUGIN_VERSION in www/common.ts to $VERSION";
 # Update common.ts
-sed -E -i "" "s/PLUGIN_VERSION = [\"'][0-9]+.[0-9]+.[0-9]+/PLUGIN_VERSION = \"$VERSION/g" www/common.ts
+sed -E -i "" "s/^  public PLUGIN_VERSION = '[0-9a-zA-Z\.-]+';/  public PLUGIN_VERSION = '$VERSION';/g" www/common.ts
 git add www/common.ts
 
 echo "Remember to update the changelog..."
