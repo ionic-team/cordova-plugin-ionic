c9ea0f90f8e5ba1a6cefc43a7ffc1504b67835b6
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2018-07-26 11:06:56 -0500

    update release process

--- bump-versions.sh
+++ bump-versions.sh
@@ -2,12 +2,10 @@
 set -f errexit
 
 # Script vars
-RELEASE_TYPE=${RELEASE_TYPE:-patch}
-RAW_VERSION=$(npm version $RELEASE_TYPE)
-VERSION=$(echo $RAW_VERSION | cut -c 2-)
-
+VERSION=$(npm list @ionic/pro-jobs | grep "@" | cut -d "@" -f 3 | cut -d " " -f 1)
 # Update common.ts
 sed -E -i "" "s/PLUGIN_VERSION = [\"'][0-9]+.[0-9]+.[0-9]+/PLUGIN_VERSION = \"$VERSION/g" www/common.ts
+git add www/common.ts
 
 # Update plugin.xml
 npm run sync-plugin-xml
