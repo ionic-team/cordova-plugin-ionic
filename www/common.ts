51c273c0e9a42bfbbf3b042b9f12a52faa7ff3db
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2018-10-05 12:04:55 -0700

    5.2.5

--- www/common.ts
+++ www/common.ts
@@ -58,7 +58,7 @@ class IonicDeployImpl {
   private _fileManager: FileManager = new FileManager();
   private SNAPSHOT_CACHE = 'ionic_built_snapshots';
   private MANIFEST_FILE = 'pro-manifest.json';
-  public PLUGIN_VERSION = '5.2.5-1';
+  public PLUGIN_VERSION = '5.2.5';
 
   constructor(appInfo: IAppInfo, preferences: ISavedPreferences) {
     this.appInfo = appInfo;
